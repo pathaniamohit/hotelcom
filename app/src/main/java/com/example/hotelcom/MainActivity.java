0a53d587fa3729126260a213c3b0f11025ef50b1
NabeelShajahan <130592372+NabeelShajahan@users.noreply.github.com>
2023-06-27 13:49:48 -0400

    Login page Updated latest

--- app/src/main/java/com/example/hotelcom/MainActivity.java
+++ app/src/main/java/com/example/hotelcom/MainActivity.java
@@ -1,43 +1,38 @@
 package com.example.hotelcom;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.hotelcom.view2;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String ARG_FROM_MAIN = "arg";
-
-
-    EditText username;
-    EditText password;
-    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText User = (EditText) findViewById(R.id.username);
+        EditText Password = (EditText) findViewById(R.id.password);
+        Button login = (Button) findViewById(R.id.loginButton);
 
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        loginButton = findViewById(R.id.loginButton);
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if (username.getText().toString().equals("") || password.getText().toString().equals("")) {
+            public void onClick(View view) {
+                if (User.getText().toString().equals("") || Password.getText().toString().equals("")) {
                     Toast.makeText(MainActivity.this, "Field Required!", Toast.LENGTH_LONG).show();
                 } else {
-                    Toast.makeText(MainActivity.this, "Login Successfully!", Toast.LENGTH_LONG).show();
-                    Intent i = new Intent(MainActivity.this, view2.class);
-                    i.putExtra(ARG_FROM_MAIN, username.getText().toString());
+                    Toast.makeText(MainActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this , view2.class);
                     startActivity(i);
                 }
+
             }
         });
     }
